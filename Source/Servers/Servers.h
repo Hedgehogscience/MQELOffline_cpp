7208cb5ab20a0358c4bab8bb05275bd1736d4d22
Convery <convery10122@gmail.com>
2018-01-11 02:21:29 +0100

    Defined the gameserver.

--- Source/Servers/Servers.h
+++ Source/Servers/Servers.h
@@ -14,3 +14,25 @@
 
 #include "Interfaces/ISSLServer.hpp"
 #include "Interfaces/IHTTPServer.hpp"
+
+// This game uses a single server for everything.
+struct Gameserver : IHTTPSServer
+{
+    // Callbacks on parsed data.
+    virtual void onGET(const size_t Socket, HTTPRequest &Request) override;
+    virtual void onPUT(const size_t Socket, HTTPRequest &Request) override;
+    virtual void onPOST(const size_t Socket, HTTPRequest &Request) override;
+    virtual void onCOPY(const size_t Socket, HTTPRequest &Request) override;
+    virtual void onDELETE(const size_t Socket, HTTPRequest &Request) override;
+
+    // Initialize the SSL certificate.
+    Gameserver(std::string_view Hostname)
+    {
+        if (!CreateSSLCert(Hostname))
+        {
+            // There's no point in even trying anymore.
+            Infoprint("Fatal error, please debug me!");
+            volatile int Dead = *(int *)0x00;
+        }
+    }
+};

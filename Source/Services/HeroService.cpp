be145896a802128e2bba74d99c45e267f24891be
Convery <Convery10122@gmail.com>
2018-01-20 09:15:32 +0100

    Reworked the character creation.

--- Source/Services/HeroService.cpp
+++ Source/Services/HeroService.cpp
@@ -12,13 +12,94 @@
 // Endpoints.
 void ChooseFirstHero(Gameserver *Server, std::string Request, std::string Body)
 {
-    MQEL_json Parsed = MQEL_json::parse(Body);
-    Infoprint(va("Selected hero %i", Parsed["heroSpecContainerId"].get<uint32_t>()));
-    World::Hero::Create((eHerotype)Parsed["heroSpecContainerId"].get<uint32_t>());
+    // Get which hero-class we use.
+    uint32_t Heroclass = MQEL_json::parse(Body)["heroSpecContainerId"];
+    Infoprint(va("Creating hero of type: %s", [Heroclass]() -> const char *
+    {
+        switch ((eHerotype)Heroclass)
+        {
+            case eHerotype::Archer:     return "Archer";
+            case eHerotype::Knight:     return "Knight";
+            case eHerotype::Mage:       return "Mage";
+            case eHerotype::Runaway:    return "Runaway";
+            default:                    return "Unknown";
+        }
+    }()));
 
-    MQEL_json Response;
-    Response["Result"] = World::Hero::Serialize();
-    Sendreply(Server, Response.dump());
+    // Create the core data for the character.
+    World::Hero::Create((eHerotype)Heroclass);
+
+    // Add gear to the character.
+    switch ((eHerotype)Heroclass)
+    {
+        case eHerotype::Archer:
+        {
+            /* TODO(Convery): Reverse-engineer this. */
+            break;
+        }
+        case eHerotype::Knight:
+        {
+            std::vector<double> Modifiers = { 0.4, 0.4, 0.4 };
+
+            // Equipment_t { ID, Sellable, Type, Level, Modifiers, Effects }
+            World::Hero::Equipitem(eItemslot::Head,         { 109, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Shoulders,    { 132, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Body,         { 110, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Hands,        { 111, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Mainhand,     { 108, false, 2, 1, Modifiers, {} });
+            break;
+        }
+        case eHerotype::Mage:
+        {
+            std::vector<double> Modifiers = { 0.4, 0.4, 0.4 };
+
+            // Equipment_t { ID, Sellable, Type, Level, Modifiers, Effects }
+            World::Hero::Equipitem(eItemslot::Head,         { 129, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Shoulders,    { 134, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Body,         { 130, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Hands,        { 131, false, 8, 1, Modifiers, {} });
+            World::Hero::Equipitem(eItemslot::Mainhand,     { 128, false, 9, 1, Modifiers, {} });
+            break;
+        }
+        case eHerotype::Runaway:
+        {
+            /* TODO(Convery): Reverse-engineer this. */
+            break;
+        }
+    }
+
+    // Return this basic character to the game.
+    Sendreply(Server, MQEL_json({"Result", World::Hero::Serialize()}).dump());
+
+    /*
+        HACK(Convery):
+        This specialization should be done after the tutorial fight.
+        In dev builds we replace the tutorial map with a 'real' map.
+    */
+    switch ((eHerotype)Heroclass)
+    {
+        case eHerotype::Archer:
+        {
+            /* TODO(Convery): Reverse-engineer this. */
+            break;
+        }
+        case eHerotype::Knight:
+        {
+            /* TODO(Convery): Reverse-engineer this. */
+            break;
+        }
+        case eHerotype::Mage:
+        {
+            World::Hero::Equipspell({ 611, 3 });
+            World::Hero::Equipspell({ 613, 0 });
+            break;
+        }
+        case eHerotype::Runaway:
+        {
+            /* TODO(Convery): Reverse-engineer this. */
+            break;
+        }
+    }
 }
 
 // Add the services to the gameserver on startup.

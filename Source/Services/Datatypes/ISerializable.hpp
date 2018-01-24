b99a7b7d423e7b4e0bdf9846c5b3cb669f4943bd
Convery <Convery10122@gmail.com>
2018-01-24 21:08:08 +0100

    Removed NOTE comment and unused struct.

--- Source/Services/Datatypes/ISerializable.hpp
+++ Source/Services/Datatypes/ISerializable.hpp
@@ -14,24 +14,7 @@ template<class K, class V, class dummy_compare, class A = std::allocator<K>>
 using MQEL_map = nlohmann::fifo_map<K, V, nlohmann::fifo_map_compare<K>, A>;
 using MQEL_json = nlohmann::basic_json<MQEL_map>;
 
-// Save the authors fingers when creating derived types.
-#define CONSTRUCT(Type)         Type() {}; Type(MQEL_json &Buffer) { Deserialize(Buffer); }; ~Type() {};
-#define DESERIALIZE(Property)   if(!Buffer[#Property].is_null()) _ ##Property = { Buffer[#Property] };
-#define SERIALIZE(Property)     Buffer[#Property] = _ ##Property;
-
-// Base structure.
-struct ISerializable
-{
-    virtual void Deserialize(MQEL_json &Buffer) {}
-    virtual MQEL_json Serialize()
-    {
-        MQEL_json Buffer = MQEL_json::object();
-        return Buffer;
-    }
-    CONSTRUCT(ISerializable);
-};
-
-// NOTE(Convery): Developer testing, remove or finalize before 1.0.
+// A simple type for request serialization.
 class Message_t
 {
     MQEL_json Internalstate;
